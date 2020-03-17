037dded33ec27debb0fcdd5a2d609334565f1f29
TracyChen33 <tracychen3621@gmail.com>
2020-03-16 20:19:04 -0600

    add404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  <div>Error enter,please check whether the address is correct</div>
+  <div>
+    <router-link to="/">back to home page</router-link>
+  </div>
+  </div></template>
+
+<script lang="ts">
+  export default {
+    name: 'NotFound'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
