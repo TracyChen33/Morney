a77fec709b6acf5d25414c6e295aeec1e4b63265
TracyChen33 <tracychen3621@gmail.com>
2020-03-17 21:17:03 -0600

    iconCreated

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,23 +2,33 @@
   <div class="nav">
     <hr/>
     <div>
-      <router-link to="/money">Money</router-link>
+      <router-link to="/money">
+        <Icon name="money"/>
+        Money
+      </router-link>
       |
-      <router-link to="/labels">Label</router-link>
+      <router-link to="/labels">
+        <Icon name="label"/>
+        Label
+      </router-link>
       |
-      <router-link to="/statistics">Statistics</router-link>
+      <router-link to="/statistics">
+        <Icon name="statistics"/>
+        Statistics
+      </router-link>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+
   export default {
     name: 'Nav'
   };
 </script>
 
 <style lang="scss" scoped>
-  .nav{
+  .nav {
     border: 1px solid red;
   }
 
