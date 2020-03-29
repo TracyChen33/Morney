982d31720e333a348745665a389f488404dfd55f
TracyChen33 <tracychen3621@gmail.com>
2020-03-29 16:03:50 -0600

    ToggleTags

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,14 +1,14 @@
 <template>
   <Layout class-prefix="layout">
     <number-pad/>
-    <Types :xxx="333"/>
+    <Types/>
     <Notes/>
-    <Tags/>
+    <Tags :data-source="tags"/>
   </Layout>
 
 </template>
 
-<script lang="ts">
+<script >
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Types from '@/components/Money/Types.vue';
   import Notes from '@/components/Money/Notes.vue';
@@ -17,6 +17,12 @@
   export default {
     name: 'Money',
     components: {Tags, Notes, Types, NumberPad},
+    data(){
+      return{
+        tags:['Cloth','Food','Home','Life'],
+      }
+    }
+
   };
 </script>
 <style lang="scss">
