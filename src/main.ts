27370c24c3d59ec7c01e49db544ca55b6bfffcae
TracyChen33 <tracychen3621@gmail.com>
2020-04-06 21:26:55 -0600

    encapsulated TagList

--- src/main.ts
+++ src/main.ts
@@ -15,16 +15,23 @@ Vue.component('Layout', Layout);
 Vue.component('Icon', Icon);
 
 window.tagList = tagListModel.fetch();
-window.createTag = (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === 'duplicated') {
-      window.alert('Label name duplicated');
-    } else if (message === 'success') {
-      window.alert('successful created');
-    }
-
+window.findTag = (id: string) => {
+  return window.tagList.filter(t => t.id === id)[0];
 };
-
+window.createTag = (name: string) => {
+  const message = tagListModel.create(name);
+  if (message === 'duplicated') {
+    window.alert('Label name duplicated');
+  } else if (message === 'success') {
+    window.alert('successful created');
+  }
+};
+window.removeTag = (id: string) => {
+  return tagListModel.remove(id);
+};
+window.updateTag = (id: string, name: string) => {
+  return tagListModel.update(id, name);
+};
 
 new Vue({
   router,
