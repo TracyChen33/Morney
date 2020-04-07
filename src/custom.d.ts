cae9599277a9d71850a2619a355c6cf2d052ba08
TracyChen33 <tracychen3621@gmail.com>
2020-04-06 21:58:11 -0600

    encapsulated RecordList

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -24,4 +24,6 @@ interface Window {
   findTag: (id: string) => Tag | undefined;
   removeTag: (id: string) => boolean;
   updateTag: (id: string, name: string) => 'success' | 'Not Found' | 'duplicated';
+  recordList: RecordItem[];
+  createRecord: (record: RecordItem) => void;
 }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,10 +18,9 @@
   import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
-  import {Component, Watch} from 'vue-property-decorator';
-  import recordListModel from '@/models/recordListModel';
+  import {Component} from 'vue-property-decorator';
 
-  const recordList = recordListModel.fetch();
+
 
 
   @Component({
@@ -29,7 +28,7 @@
   })
   export default class Money extends Vue {
     tags = window.tagList;
-    recordList: RecordItem[] = recordList;
+    recordList = window.recordList;
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0 //Record的初始值
     };
@@ -43,12 +42,7 @@
     }
 
     saveRecord() {
-      recordListModel.create(this.record);
-    }
-
-    @Watch('recordList')
-    onRecordListChange() {
-      recordListModel.save();
+      window.createRecord(this.record);
     }
 
   }
