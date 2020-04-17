23bcc81fe938e9247a40919011721008a1d01661
TracyChen33 <tracychen3621@gmail.com>
2020-04-16 19:38:58 -0600

    Tags.vue use Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -15,12 +15,20 @@
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
-  import store from '@/store/index2';
 
-  @Component
+  @Component({
+    computed:{
+      tagList(){
+        return this.$store.state.tagList;
+
+      }
+    }
+  })
   export default class Tags extends Vue {
-tagList = store.fetchTags();
     selectedTags: string[] = [];
+    created(){
+      this.$store.commit('fetchTags');
+    }
     toggle(tag: string) {
       const index = this.selectedTags.indexOf(tag);
       if (index >= 0) {
@@ -33,7 +41,8 @@ tagList = store.fetchTags();
     create() {
       const name = window.prompt('please enter your Tags name');
       if (!name) {return window.alert('Tags cannot be null');}
-        store.createTag(name);
+
+      this.$store.commit('createTag',name);
     }
   }
 </script>
