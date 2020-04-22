742a7db1a03b16b635f7e8d0ebcd3e35a8a644d7
TracyChen33 <tracychen3621@gmail.com>
2020-04-21 23:31:52 -0600

    Table

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,26 +2,42 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-  <div>
-    type:{{type}}
-    <br/>
-    interval:{{interval}}
-  </div>
+    <div>
+      type:{{type}}
+      <br/>
+      interval:{{interval}}
+      <br/>
+    </div>
+    <div>
+      <ol>
+        <li v-for="(group,index) in result" :key="index">
+          <h3>{{group.title}}</h3>
+          <ol>
+            <li v-for="item in group.items" :key="item.id">
+              {{item.amount}} {{item.createdAt}}
+            </li>
+          </ol>
+        </li>
+      </ol>
+    </div>
   </Layout>
 </template>
 <style scoped lang="scss">
   ::v-deep .type-tabs-item {
     background: white;
+
     &.selected {
       background: #c4c4c4;
+
       &::after {
         display: none;
       }
     }
   }
-::v-deep .interval-tabs-item{
-  height: 48px;
-}
+
+  ::v-deep .interval-tabs-item {
+    height: 48px;
+  }
 
 
 </style>
@@ -39,9 +55,30 @@
   })
 
   export default class Statistics extends Vue {
+    get recordList() {
+      return (this.$store.state as RootState).recordList;
+    }
+
+    get result() {
+      const {recordList} = this;
+      type HashTableValue = { title: string;items: RecordList[] }
+      const hashTable: { [key: string]: HashTableValue } = {};
+      for (let i = 0; i < recordList.length; i++) {
+        const [date, time] = recordList[i].createdAt!.split('T');
+        hashTable[date] = hashTable[date] || {title: date, items: []};
+        hashTable[date].items.push(recordList[i]);
+      }
+      console.log(hashTable);
+      return hashTable;
+    }
+
+    beforeCreate() {
+      this.$store.commit('fetchRecords');
+    }
+
     type = '-';
     interval = 'day';
-    intervalList=intervalList;
-    recordTypeList =recordTypeList;
+    intervalList = intervalList;
+    recordTypeList = recordTypeList;
   }
 </script>
